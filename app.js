45e826cbb5c041d87a6fb0bc10ab80c27d95c22d
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2024-12-04 04:29:09 +0100

    alertas que salen de popup en la página web

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const shadowsObj = require('./utilsShadows.js')
-const webSockets = require('./utilsWebSockets.js')
-const port = 3000
+// const shadowsObj = require('./utilsShadows.js')
+// const webSockets = require('./utilsWebSockets.js')
+const port = 8080
 
+const app = express()
+app.listen(port)
 // Publicar arxius carpeta ‘public’ 
 app.use(express.static('public'))
 
--- public/index.js
+++ public/index.js
@@ -1,1 +1,66 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let btn = document.getElementById("btn");
+    btn.addEventListener("click", userInfo);
+});
 
+function userInfo() {
+    // Coger elementos del server
+    let serverInput = document.getElementById("server");
+    // let parent = serverInput.closest(".entryarea");
+    // let labelText = serverParent.querySelector(".labelline")?.textContent.trim();
+    // Coger elementos del puerto
+    let portInput = document.getElementById("port");
+    //Coger elementos del nombre
+    let nameInput = document.getElementById("name");
+
+    String.prototype.isAlpha = function() {
+        return /^[a-zA-Z]+$/.test(this);
+    };
+    
+
+    if (serverInput.value.trim().toLowerCase() !== "localhost") {
+        Swal.fire({
+            title: "Oops...",
+            heightAuto: false,
+            text: 'Por favor, asegúrate de que el servidor sea localhost. Este sistema está diseñado para ejecutarse exclusivamente en un entorno local.',
+            icon: 'error',
+            button: '',
+            customClass: {
+                container: 'swal-custom-container'
+            }
+        });
+    } else if (portInput.value.trim() !== "8888") {
+        Swal.fire({
+            title: "Oops...",
+            heightAuto: false,
+            text: 'Parece que has ingresado un puerto no válido. Por favor, asegúrate de usar el puerto 8888 para acceder correctamente.',
+            icon: 'error',
+            button: '',
+            customClass: {
+                container: 'swal-custom-container'
+            }
+        });
+    } else if (!/^[a-zA-Z]+$/.test(nameInput.value.trim())) {
+        Swal.fire({
+            title: "Oops...",
+            heightAuto: false,
+            text: 'Parece que has ingresado un nombre no válido. Por favor, asegúrate de que el nombre contenga solo letras.',
+            icon: 'error',
+            button: '',
+            customClass: {
+                container: 'swal-custom-container'
+            }
+        });
+    } else {
+        Swal.fire({
+            title: "Correcto",
+            heightAuto: false,
+            text: 'Has puesto todos los datos correctamente, ahora se te llevará a la siguiente página donde deberás esperar hasta conectar para jugar contra otro usuario.',
+            icon: 'success',
+            button: '',
+            customClass: {
+                container: 'swal-custom-container'
+            }
+        });
+    }
+}    
